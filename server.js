f477c33e90ff83aa15f3084d10642f899bb0933c
Puru Dahal <pdahal@me.com>
2014-10-09 13:14:16 -0700

    Setup port and start the server

--- server.js
+++ server.js
@@ -2,3 +2,10 @@
 var express         = require('express')
 var mongoose        = require('mongoose')
 var app             = express()
+
+// Setup port
+var port = process.env.PORT || 8080
+
+// Start app
+app.listen(port)
+console.log('Feedle loading on: http://localhost:' + port)
