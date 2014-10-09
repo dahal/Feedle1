0a166c0332bd5ad55d7e9d752c3f1457191d8809
Puru Dahal <pdahal@me.com>
2014-10-09 14:41:31 -0700

    Configure routes and public assets pipeline

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ var express         = require('express')
 var mongoose        = require('mongoose')
 var app             = express()
 
+// Setup static file location
+app.use(express.static(__dirname+'/public'))
+
+// Routes
+require('./routes')(app)
+
 // Setup port
 var port = process.env.PORT || 8080
 
